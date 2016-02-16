aef3285679cf2657ce774f735d04e9ce7955e3e6
Allison <pencils_zoom11@hotmail.com>
2016-02-15 22:33:20 -0500

    Update psalms.py
    added more comments, fixed some things

--- code/monk-simulator/psalms.py
+++ code/monk-simulator/psalms.py
@@ -1,20 +1,24 @@
 
 def getPsalms():  #function that gets a list of all the psalms. You must have a text file of all the psalms in the same directory for it to work however.
 
-	psalms = [] #array that holds an array of each psalm
-	verses = [] 
+	psalms = [] #list that will hold all the psalms
+	verses = [] #list that holds each chapter of the psalms, verse by verse
 
-	psalmstxt = open('psalms.txt', 'r')
+	psalmstxt = open('psalms.txt', 'r') #opens text file with all the psalms
 
-	for line in psalmstxt:
+	for line in psalmstxt: #reads each line
 
-		end = len(line) - 1 #keeps track of the end of the psalm
+		end = len(line) - 1 #each verse is a line. This is the index of the end of the verse.
 
-		if ":1 " in line:
+		if ":1 " in line: #looks at the verse of the psalm. ex: 121:4
+		#if it is the 1st verse, then it must be a new psalm!
 			
-			psalms.append(verses)
-			verses = []
-			start = line.find(" ")
+			psalms.append(verses) #since a new psalm is beginning, we append the old one to the master list
+			verses = [] #resets verses since it's beginning again
+			
+			#gets rid of the verse numbers at the beginning of the line
+			start = line.find(" ")  
+			#adds verse to list
 			verses.append(line[start+1:end])
 			
 		else:
@@ -22,21 +26,21 @@ def getPsalms():  #function that gets a list of all the psalms. You must have a 
 			verses.append(line[start+1:end])
 			
 
-	psalmstxt.close()
+	psalmstxt.close() #gotta remember to close those files!
 
 
-	return psalms
+	return psalms #returns list of all psalms.
 	
 	
 	
 
 	
 
-def say_psalms(number,boolPrompt, psalmsArray):
+def say_psalms(number,boolPrompt, psalmsArray): #arguments: number of psalms, whether you want a prompt, and the array with all the psalms
 
-	print("Please say psalm number", str(number) + ".\n0")
+	print("Please say psalm number", str(number) + ".\n")
 
-	for line in psalmsArray[number]:
+	for line in psalmsArray[number]: #goes through the given psalm line by line.
 		if boolPrompt == 1:
 			print("\n",line)
 		
@@ -56,7 +60,7 @@ def main():
 
 	psalmsArray = getPsalms()
 	
-	get_number = int(input("Which psalm would you like to say?"))
+	get_number = int(input("Which psalm would you like to say?")) #should probably add error checking for this...
 
 	while True: 
 		prompt = input("\nWould you like a prompt? Enter 1 for yes and 0 for no.")
