aab357c2b9470b64b5e73d52633c0e8fe593fe8d
Allison <pencils_zoom11@hotmail.com>
2016-03-19 21:43:33 -0400

    Update monkSimulator.py
    added in journey random event

--- code/monk-simulator/monk-beta-1/monkSimulator.py
+++ code/monk-simulator/monk-beta-1/monkSimulator.py
@@ -1,14 +1,13 @@
 import player
 import chronology
 import activities
+from random import randint
 
 '''
 TO-DO
 
 - more romance
-- epic journey
-- interaction with abbot?
-- private mass?
+- more contemporary events
 
 '''
 
@@ -55,10 +54,7 @@ def main():
 	you = player.Player(name)
 
 	print("Welcome", name + "! You are now a monk.")
-	
-	#load(player, time, "matins")
-	
-	
+
 	
 	while you.alive == True:
 
@@ -66,14 +62,21 @@ def main():
 		
 		print(time.getTotalDays())
 		
-		print("A new day begins! Early in the morning, before even the light of day has broken over the fertile plains of Burgundy, the monks of Cluny rise from their slumber to begin their daily rounds of prayer.")
+		if you.journey == False:
+			print("A new day begins! Early in the morning, before even the light of day has broken over the fertile plains of Burgundy, the monks of Cluny rise from their slumber to begin their daily rounds of prayer.")
 
-		if you.isSick() == False:	
-			normal_day(you)
-		else:
-			sick_day(you)
-		#print("day over")
+			if you.journey == True:
+				journey(you)
 			
+			if you.isSick() == False:	
+				normal_day(you)
+			else:
+				sick_day(you)
+			#print("day over")
+		else:
+			journey(you)
+		
+		
 		time.dayEnd(you)
 		
 		
@@ -143,4 +146,88 @@ def normal_day(player):
 			else:
 				activity.skip = True
 
+def journey(player):
+	
+	next = input("Press enter to continue on your journey.")
+	print("You continue on your journey.")
+	
+	if player.journeyDays < 1:
+		print("You are travelling across Burgandy. It's pretty scenic.")
+		
+	if player.journeyDays == 1:
+		print("You are walking across the alps. It's a little bit rocky.")
+		
+	if player.journeyDays == 2:
+		print("You are in Italy.")
+		
+	if player.journeyDays == 3:
+		print("You arrive in Rome and deliver the message to the Pope.")
+		print("After staying for a while in Rome, you decide it's time to go back.")
+		
+		
+	rand = randint(0,100)
+	
+	if rand < 10:
+		print("Oh no! You've been captured by brigands! What do you do?")
+		print("1: Fight them.")
+		print("2: Pray")
+		
+		while True:
+			action = input("> ").lower()
+			
+			if action == "fight them" or action == "1":
+				print("You fight against the bandits, but all they do is beat you up more. They leave you on the side of the road, nearly dead.")
+				player.changeHealth(3)
+				break
+				
+			elif action == "pray" or action == "2":
+				print("You don't fight back against the bandits, and instead remain silent and pray to God. Miraculously, when the bandits try to attack you, they are repelled by a mysterious force. They are so in awe that they all decide to become monks. They let you go unharmed.")
+				player.decreasePenance(2)
+				break
+			else: 
+				print("I don't understand that!")
+				
+	elif rand < 20:
+		print("You stop at a monastary on the road. They offer you some delicious looking chicken for dinner. Should you eat it?")
+		
+		while True:
+			action = input("> ").lower()
+			
+			if action == "yes":
+				print("You eat the meat.")
+				player.increaseSins("laxity")
+				break
+			elif action == "no":
+				print("You don't eat the meat! Good call!")
+				player.decreasePenance(1)
+				break
+			else: 
+				print("Please answer either yes or no!")
+				
+	elif rand < 30:
+		print("You stop in an inn on the way to rest. While there, you notice a beautiful women looking at you. She approaches you.")
+		print('"Hey there, handsome," she says, "Wanna have a good time?" She waggles her eyebrows at you.')
+		print("You....")
+		print("1. Have a good time.")
+		print("2. Don't have a good time.")
+		
+		while True:
+			action = input("> ").lower()
+			
+			if action == "1" or action == "have a good time":
+				print("You have a good time.")
+				player.increaseSins("kissing")
+				break
+			if action == "2" or action == "don't have a good time":
+				print('"Get behind me Satan!", you cry. The woman looks insulted and walks away. Phew. You narrowly got out of that one.')
+				break
+			else:
+				print("I don't understand that.")
+		
+	if player.journeyDays == 7:
+		print("After a long and arduous trek, finally the great walls of Cluny come into sight once more! You did it!")
+		player.journey = False
+	
+	player.journeyDays += 1
+	
 main()
