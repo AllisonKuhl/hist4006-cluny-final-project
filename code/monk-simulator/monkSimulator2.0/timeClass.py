1dc5b501cfcad243d0e57f373e82cf23c626c9ef
Allison <pencils_zoom11@hotmail.com>
2016-04-06 23:03:36 -0400

    Update timeClass.py

--- code/monk-simulator/monkSimulator2.0/timeClass.py
+++ code/monk-simulator/monkSimulator2.0/timeClass.py
@@ -3,11 +3,11 @@ import activityClass
 liturgy = activityClass.Prayer()
 sleep = activityClass.Sleep()
 dinner = activityClass.Eat()	
-			
+chapter = activityClass.Chapter()
 
 divineHours = ["nocturnes", "matins", "prime", "terce", "sext", "nones", "vespers", "compline"]	
 
-freeTime =  activityClass.Activity("free time.","","",True, "You have some free time now. Better enjoy it!")
+freeTime =  activityClass.Activity("free time.","","", "You have some free time now. Better enjoy it!")
 
 class Time():
 	def __init__(self):
@@ -15,7 +15,7 @@ class Time():
 		self.totalTurns = 0
 		self.turns = 0
 		self.activityIndex = 0
-		self.activities = ["nocturnes", "free time", "matins", "sleep", "prime", "freeTime", "terce", "chapter meeting", "free time", "sext", "nones", "dinner", "sleep", "freeTime", "vespers", "compline"]
+		self.activities = ["nocturnes", "free time", "matins", "sleep", "prime", "free time", "terce", "chapter meeting", "free time", "sext", "nones", "dinner", "sleep", "free time", "vespers", "compline"]
 		
 	def getCurrentActivityName(self):
 		return self.activities[self.activityIndex]
@@ -27,22 +27,56 @@ class Time():
 		if activity in divineHours:
 			return liturgy
 		
-		if activity == sleep.name:
+		elif activity == sleep.name:
 			return sleep
 			
-		if activity == dinner.name:
+		elif activity == dinner.name:
 			return dinner
 			
-		if activity == "free time":
+		elif activity == "free time":
 			return freeTime
 			
-	def endTurn(self):
+		elif activity == "chapter meeting":
+			return chapter
+	
+		else:
+			return ""
+		
+	def getCurrentActivityType(self):
+	
+		activity = self.activities[self.activityIndex]
+		
+		if activity in divineHours:
+			return "liturgy"
+			
+		else:
+			return activity.name
+		
+	def resetTurn(self):
+		self.turns = -1
+		if self.activityIndex < len(self.activities):
+			self.activityIndex += 1
+		
+	def endTurn(self, player):
+		
+		print("INDEX = ", self.activityIndex)
+		print("DAY NUMBER = ", self.day)
 		
 		self.totalTurns += 1
 		self.turns += 1
 		if self.turns > 15:
 			self.turns = 0 
+			
+			if self.getCurrentActivityName() != "free time":
+				next = input("Woops! You've missed " + self.getCurrentActivityName() + "!")
+				player.increaseSins("skipping")
+			
 			self.activityIndex += 1
+			
+			
 		if self.activityIndex >= len(self.activities)-1:
-			print("The day is over!")
+			print("You have finished the day. Let's begin the next!")
+			self.day += 1
+			self.activityIndex = 0
+			self.turns = 0
 			
