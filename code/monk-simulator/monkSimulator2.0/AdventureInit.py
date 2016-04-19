2851785f2d371313b27727e241d989732ef6151b
Allison <pencils_zoom11@hotmail.com>
2016-04-19 13:49:30 -0400

    Update AdventureInit.py

--- code/monk-simulator/monkSimulator2.0/AdventureInit.py
+++ code/monk-simulator/monkSimulator2.0/AdventureInit.py
@@ -9,18 +9,18 @@ class Abbey():
 		
 		
 		#creates each room
-		church = Room("church", "You are in the sanctuary of the church. It's superflous heights and vapid flourishes are astounding. This is the currently the biggest church in Europe!")
+		church = Room("church", "You are in the sanctuary of the church. It's very nice. This is the currently the biggest church in Europe!")
 		galilee = Room("galilee", "You are in the apse of the church. The altar is further back.")
 		narthex = Room("narthex", "This is the entrance of the church.")
 		passage1 = Room("passage", "You are in the galilee passageway")
-		court = Room("court", "You are in the abbot's court.")
+		court = Room("court", "You enter the courtyard of the abbatial palace within the abbatial palace built by Saint Hugh. This open-air space is the centre of the abbatial palace, formerly the atrium to the second church of Peter and Paul (Cluny II). Around you visitors and monastic officers speak and negotiate. The possibility of failing to keep your proper monastic silence and equanimity seems greater in this mundane space.")
 		chapel = Room("chapel", "You find yourself in a small chapel.")
 		atrium = Room("atrium", "You are in an atrium.")
-		cloister = Room("cloister", "You are in the cloister. This is heart of the abbey!")
-		hall = Room("hall", "You are in a hallway.")
+		cloister = Room("cloister", "Passing through the covered arcades surrounding the cloister, you enter into the verdant space of the cloister garden - open to the sun and the elements. You rejoice in the great vision of Saintly Odilo, who rebuilt the cloister and turned the space into a veritable new Eden. In summer time, you find nothing more enjoyable than to soak up the warmth of the sun on your face and tend the medicinal or culinary herbs that grow in their neatly arranged beds.  In winter, the rain makes it less hospitable, but the gardens still fill with greenery except from Advent until Epiphany. With books in hand, monks will often sit on the low wall in between the brilliant marble pillars supporting the open galleries running alongside the surrounding buildings. At the south-east end of the cloister a single monk looks down from the dormitory landing. In the south-west corner of the cloister is the covered washing fountain, bubbling with water fed from a nearby spring. This is where all the monks wash and cleanse themselves before mass and before entering the refectory.")
 		cellar = Room("cellar", "You are in the cellar. It is full of delicious looking food.")
 		kitchen = Room("kitchen", "You are in the kitchen.")
 		refectory = Room("refectory", "You are in the refectory. This is where the monks eat.")
+		hall = Room("hall", "You are in a hallway")
 		passage2 = Room("passage", "You are in a long passageway.")
 		infirmary = Room("infirmary", "You are in the infirmary. It's pretty big!")
 		cemetary = Room("cemetary", "You are in the cemetary. Spooky! Everyone wants to be buried here.")
@@ -29,7 +29,7 @@ class Abbey():
 		latrines1 = Room("latrines", "You are in the latrines. It's kinda smelly.")
 		hall2 = Room("hall", "You are in a hallway.")
 		calefactorum = Room("calefactorum", "As you enter the room, you are suprised by the emenating heat. There is a large fire burning in the room.")
-		chapter = Room("chapter", "You are in chapter. This is where the monks meet to discuss important matters of business.")
+		chapter = Room("chapter", "The intimate space of the Chapter room is warmed by the light coming through the raised windows – three on the north side and fourth on the east. It is in this space that the monks gather to hear the Rules of the monastery and discuss monastery business. Though empty now, soon it will be filled with all the members of the community to admit openly (or face accusations) of their failings in the monastic life.")
 		
 		#upstairs
 		dormitory = Room("dormitory", "You are in the dormitory. The room is filled with simple cots.")
@@ -62,7 +62,7 @@ class Abbey():
 		novitiat.addRooms(latrines1,None, None, noviceCloister)
 		latrines1.addRooms(None, None, novitiat, None)
 		calefactorum.addRooms(None,None, refectory, hall2)
-		chapter.addRooms(None,hall2,None,None,None)
+		chapter.addRooms(infirmary,hall,cloister,church)
 
 		#add stairs
 		hall2.stairs = dormitory
@@ -92,9 +92,10 @@ class Abbey():
 		novitiat.addObject(bed)
 		
 		
-		self.startRoom = chapter
+		self.startRoom = novitiat
 		
-		
+		print("hello world")
+		print(cloister.description)
 		
 		#add people
 		
